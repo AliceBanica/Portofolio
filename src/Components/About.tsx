6d059272cd401909fa9fce04abb2b9e82f16ca5f
Alice Banica <banica.alice@gmail.com>
2023-02-03 22:55:14 +0200

    first version of experience description

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,12 +5,40 @@ import './About.css';
 import location from '../Images/location.png';
 
 
-const Experience = () =>{
+const Experience = ({newHeight}:{newHeight:any}) =>{
+    const [infoClicked, setInfoClicked] = useState<number>(0);
+    const [showInfoDiv, setShowInfoDiv] = useState<boolean>(false);
 
     let expMap = new Map([[1, 'school'],[2,'michelin'],[3,'master'], [4,'philip'], [5,'bachelor']]);
+    let expDescMap = new Map([['school', false],['michelin', false], ['master', false], ['philip', false], ['bachelor', false]]);
 
-    const handleClickExp = (param:number) =>{
-        console.log(expMap.get(param))
+    const resetExpDescMap = ()=>{
+        expDescMap.forEach((v, k) => v = false);
+    }
+    
+
+    const handleClickState=(param:any) =>{
+        setInfoClicked(param);
+        const newHeightDiv = true;  
+        setShowInfoDiv(true);
+        newHeight(newHeightDiv);
+
+        // let selectedExp:any = expMap.get(param); 
+
+        // if(!expDescMap.get(selectedExp)){
+        //     expDescMap.set(selectedExp,true);
+        //     console.log(expDescMap.get(selectedExp))
+        //     console.log(expDescMap);
+        //     resetExpDescMap();
+        //     const newHeightDiv = true;  
+        //     setShowInfoDiv(true);
+        //     newHeight(newHeightDiv);
+        // } else {
+        //     expDescMap.set(selectedExp, false);
+        //     const newHeightDiv = false;  
+        //     setShowInfoDiv(false);
+        //     newHeight(newHeightDiv);
+        // }
     }
 
     return <section className='experience-container'>
@@ -18,19 +46,19 @@ const Experience = () =>{
                     <div className='experience-left'>
                         <ul className='experience-list' >
                             <li>
-                                <div className='about-see-more about-see-more-first' onClick={()=>handleClickExp(1)}>?</div> 
+                                <div className='about-see-more about-see-more-first' onClick={()=>{handleClickState(1)}}>?</div> 
                                 Scoala Informala de IT - Front-end Web Development
                                 <p className='experience-period'>period</p>
                             </li>
-                            <li>&nbsp;</li>
+                            <li className='not-li'>&nbsp;</li>
                             <li className='third-li'>
-                                <div className='about-see-more about-see-more-third'  onClick={()=>handleClickExp(3)}>?</div> 
+                                <div className='about-see-more about-see-more-third'  onClick={()=>{handleClickState(3)}}>?</div> 
                                 Master's Degree - Applied Informatics in Electrical Engineering (UPB)
                                 <p className='experience-period'>period</p>
                             </li>
-                            <li>&nbsp;</li>
+                            <li className='not-li'>&nbsp;</li>
                             <li className='fifth-li'>
-                                <div className='about-see-more  about-see-more-fifth' onClick={()=>handleClickExp(5)}>?</div> 
+                                <div className='about-see-more  about-see-more-fifth' onClick={()=>{handleClickState(5)}}>?</div> 
                                 Bachelor's Degree - Applied Informatics in Electrical Engineering (UPB)
                                 <p className='experience-period'>period</p>
                             </li> 
@@ -38,23 +66,36 @@ const Experience = () =>{
                     </div>
                     <div className='experience-right'>
                         <ul className='bar'>
-                            <li>&nbsp;</li>
+                            <li className='not-li'>&nbsp;</li>
                             <li>
-                                <div className='about-see-more  about-see-more-second' onClick={()=>handleClickExp(2)}>?</div> 
+                                <div className='about-see-more  about-see-more-second' onClick={()=>{handleClickState(2)}}>?</div> 
                                 Quality Engineer - Michelin
                                 <p className='experience-period period-right'>period</p>
                             </li>
-                            <li>&nbsp;</li>
+                            <li className='not-li'>&nbsp;</li>
                             <li>
-                                <div className='about-see-more  about-see-more-fourth' onClick={()=>handleClickExp(4)}>?</div> 
+                                <div className='about-see-more  about-see-more-fourth' onClick={()=>{handleClickState(4)}}>?</div> 
                                 Manufacturing Trainee - Philip &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Morris Romania
                                 <p className='experience-period period-right'>period</p>
                             </li>
-                            <li className='fifth-li'>&nbsp;</li> 
+                            <li className='fifth-li not-li'>&nbsp;</li> 
                         </ul>
                     </div>
                 </div>
+                
+                {showInfoDiv && <ExperienceInfo idInfo = {infoClicked} message = {expMap.get(infoClicked)} showInfo = {showInfoDiv}/>}
             </section>
+}
+
+const ExperienceInfo = (props:any) =>{
+    // return <div className='experience-more-info'>{props.idInfo} {props.message}</div>
+    // return <div className='experience-more-info'>
+    //         {props.idInfo} {props.message}
+    //     </div>
+
+        return <div className={clsx('experience-more-info', props.showInfo && 'show-div')}>
+            {props.idInfo} {props.message}
+        </div>
 }
 
 const AboutMe = () =>{
@@ -72,11 +113,16 @@ const About = () => {
     const [text, setText] = useState<string>('');
     const isAboutMeActive = text === 'aboutMeDescription' || text === '';
     const isMyExperienceActive = text === 'myExperienceDescription';
+    const [heightState, setHeightState] = useState<boolean>(false);
 
-    
+    const newHeight = (heightState:boolean) =>{
+        setHeightState(heightState);
+    }
 
     const handleClickAbout = () => {
         setText('aboutMeDescription');
+        const newHeightDiv = false; 
+        newHeight(newHeightDiv);
     }
 
     const handleClickExperience = () => {
@@ -102,9 +148,10 @@ const About = () => {
                             </li>
                         </ul>
                     </div>
-                    <div className='description-right'>
+                    {/* <div className='description-right'> */}
+                    <div className={clsx('description-right', heightState && 'more-info-active' )}> 
                         {isAboutMeActive && <AboutMe/>}
-                        {isMyExperienceActive && <Experience/>}
+                        {isMyExperienceActive && <Experience newHeight= {newHeight}/>}
                     </div>
                 </div>
             </div>
